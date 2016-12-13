aa26e1e3567bb484685d00379680502d33efcca0
Lewis Jang <lewis.aedes@gmail.com>
2016-12-13 14:16:16 +0800

    add frames merge function

--- modules/ljFrames.js
+++ modules/ljFrames.js
@@ -1,5 +1,38 @@
 var frames = {};
 
+/**
+ * 兩個三圍影格
+ *
+ * @param array a [{x: float, y: float, z: float}... ]
+ * @param array b [{x: float, y: float, z: float}... ]
+ *
+ * @return array [{x: float, y: float, z: float}... ]
+ */
+frames.merge = function (a, b) {
+    var xyz = require('ljXYZ');
+    var frameTotalIndex = Math.max(a.length, b.length);
+    var frames = [];
+
+    for (var i = 0; i < frameTotalIndex; i++)
+    {
+        if (a[i] && b[i])
+        {
+            var c = xyz.merge(a[i], b[i]);
+            frames.push(c);
+        }
+        else if (a[i])
+        {
+            frames.push(a[i]);
+        }
+        else
+        {
+            frames.push(b[i]);
+        }
+    }
+
+    return frames;
+};
+
 var functionsName = Object.keys(frames);
 
 for (var i = 0; i < functionsName.length; i++)
